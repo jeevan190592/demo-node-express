2c531ff59565e76749adf4288d598ed5e45fbcc1
jeevan190592 <jeevan190592@gmail.com>
2021-02-11 23:07:08 +0100

    demo crud operation is done

--- index.js
+++ index.js
@@ -1,11 +1,73 @@
+
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+const courses = [
+  {id:1, name: 'course1'},
+  {id:2, name: 'course2'},
+  {id:3, name: 'course3'}
+
+]
 app.get('/', (req, res) => {
   res.send('hello world');
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1,2,3]);
+    res.send(courses);
 });
-app.listen(3000, () => console.log('listening on port 3000...'));
+
+app.post('/api/courses', (req,res) => {
+  const {error} = validateCourse(req.body); //result.error
+  if(error) return res.status(400).send(error.details[0].message);
+
+const course = {
+  id: courses.length + 1,
+  name: req.body.name
+};
+courses.push(course);
+res.send(course);
+});
+
+app.put ('/api/courses/:id', (req,res) =>{
+
+const course = courses.find(c => c.id === parseInt(req.params.id ));
+if (!course) return res.status(404).send('the course with given name is not found.');
+ 
+const {error} = validateCourse(req.body); //result.error
+if(error) return res.status(400).send(error.details[0].message);
+
+course.name = req.body.name;
+res.send(course);
+
+});
+
+
+
+function validateCourse(course) {
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+  
+  return Joi.validate(course, schema);
+}
+app.get('/api/courses/:id', (req,res) => {
+ const course = courses.find(c => c.id === parseInt(req.params.id ));
+ if (!course) return res.status(404).send('the course with given is not found.');
+ res.send(course);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on port ${port}...`));
+
+app.delete('/api/courses/:id', (req,res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id ));
+ if (!course) return res.status(404).send('the course with given name is not found.');
+
+const index = courses.indexOf(course);
+courses.splice(index, 1);
+
+res.send(course);
+});
